8489393702b30918c3684167e72a08bdb869743b
Simon Taylor <simon.taylor@spt-development.com>
2022-05-20 21:51:25 +0100

    Updated dependencies to align with Spring Boot 2.7.0, moved JDK version used for build to JDK 18 and updated plugins to enable JDK 18 support

--- spt-development-cid-jms-spring-boot-autoconfigure/src/main/java/com/spt/development/cid/jms/spring/boot/autoconfigure/CidJmsSpringAutoConfiguration.java
+++ spt-development-cid-jms-spring-boot-autoconfigure/src/main/java/com/spt/development/cid/jms/spring/boot/autoconfigure/CidJmsSpringAutoConfiguration.java
@@ -1,18 +1,17 @@
 package com.spt.development.cid.jms.spring.boot.autoconfigure;
 
 import com.spt.development.cid.jms.spring.CorrelationIdSetter;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.jms.annotation.JmsListener;
 
 /**
- * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-Configuration} for
+ * {@link AutoConfiguration Auto-Configuration} for
  * <a href="https://github.com/spt-development/spt-development-cid-jms-spring">spt-development/spt-development-cid-jms-spring</a>.
  */
-@Configuration
+@AutoConfiguration
 @ConditionalOnClass({ JmsListener.class })
 public class CidJmsSpringAutoConfiguration {
 
